49029e9078a98758cf10579c67c034d03f2dc826
GeekMakerSun <geek@13aq.com>
2022-11-24 15:07:40 +0800

    修改 汉化 批量 应该没问题

--- src/components/form/js/fn/controls-disable.js
+++ src/components/form/js/fn/controls-disable.js
@@ -1,18 +1,25 @@
+
 /*
  * Disabled Controls
  *
  * @param  {String} controls                 - Wrapper of controls.
  * @return {Void}
  */
-(function($) {
+( function ( $ ) {
     'use strict';
-    $.fn.UixRenderControlsDisable = function(options) {
+    $.fn.UixRenderControlsDisable = function( options ) {
+ 
         // This is the easiest way to have default options.
         const settings = $.extend({
-            controls: 'input.is-disabled'
-        }, options);
-        this.each(function() {
-            $(settings.controls).prop('disabled', true);
-        });
+			controls    : 'input.is-disabled'
+        }, options );
+ 
+        this.each( function() {
+		
+			$( settings.controls ).prop( 'disabled', true );
+			
+		});
+ 
     };
-}(jQuery));
+ 
+}( jQuery ));
