3e2168156b34ee5e83f996a364302749a6674cfa
GeekMakerSun <geek@13aq.com>
2022-11-27 18:05:04 +0800

    修改 首页

--- src/components/fullwidth-column-to-edge/js/index.js
+++ src/components/fullwidth-column-to-edge/js/index.js
@@ -1,7 +1,6 @@
-
-/* 
+/*
  *************************************
- * <!-- Full Width Column to Edge -->
+ * <!-- 全宽列到边缘 Full Width Column to Edge -->
  *************************************
  */
 
@@ -22,83 +21,80 @@ import {
 import '../scss/_style.scss';
 
 
-
-export const FULL_WIDTH_COLUMN_TO_EDGE = ( ( module, $, window, document ) => {
-	if ( window.FULL_WIDTH_COLUMN_TO_EDGE === null ) return false;
-	
-	
-    module.FULL_WIDTH_COLUMN_TO_EDGE               = module.FULL_WIDTH_COLUMN_TO_EDGE || {};
-    module.FULL_WIDTH_COLUMN_TO_EDGE.version       = '0.0.2';
-    module.FULL_WIDTH_COLUMN_TO_EDGE.pageLoaded    = function() {
-
-		$( '.js-uix-fullwidth-column-to-edge--extend-right' ).each( function()  {
-			const $el = $( this );
-			const actived = $el.data( 'activated' );
-			if( typeof actived === typeof undefined ) {
-
-				fullwidthToDir( $( this ), 'right', window.innerWidth );
-
-				//Prevents front-end javascripts that are activated in the background to repeat loading.
-				$el.data( 'activated', 1 );
-			}//endif actived
-		});
-
-		$( '.js-uix-fullwidth-column-to-edge--extend-left' ).each( function()  {
-			const $el = $( this );
-			const actived = $el.data( 'activated' );
-			if( typeof actived === typeof undefined ) {
-
-				fullwidthToDir( $( this ), 'left', window.innerWidth );
-
-				//Prevents front-end javascripts that are activated in the background to repeat loading.
-				$el.data( 'activated', 1 );
-
-			}//endif actived
+export const FULL_WIDTH_COLUMN_TO_EDGE = ((module, $, window, document) => {
+    if (window.FULL_WIDTH_COLUMN_TO_EDGE === null) return false;
 
 
-		});
-		
-		
-        function fullwidthToDir( obj, dir, w ) {
+    module.FULL_WIDTH_COLUMN_TO_EDGE = module.FULL_WIDTH_COLUMN_TO_EDGE || {};
+    module.FULL_WIDTH_COLUMN_TO_EDGE.version = '0.0.2';
+    module.FULL_WIDTH_COLUMN_TO_EDGE.pageLoaded = function () {
+
+        $('.js-uix-fullwidth-column-to-edge--extend-right').each(function () {
+            const $el = $(this);
+            const actived = $el.data('activated');
+            if (typeof actived === typeof undefined) {
+
+                fullwidthToDir($(this), 'right', window.innerWidth);
+
+                //Prevents front-end javascripts that are activated in the background to repeat loading.
+                $el.data('activated', 1);
+            }//endif actived
+        });
+
+        $('.js-uix-fullwidth-column-to-edge--extend-left').each(function () {
+            const $el = $(this);
+            const actived = $el.data('activated');
+            if (typeof actived === typeof undefined) {
+
+                fullwidthToDir($(this), 'left', window.innerWidth);
+
+                //Prevents front-end javascripts that are activated in the background to repeat loading.
+                $el.data('activated', 1);
+
+            }//endif actived
+
+
+        });
+
+
+        function fullwidthToDir(obj, dir, w) {
             const dividerPosition = obj.offset();
-            const dividerWidth = $( window ).width() - dividerPosition.left;
+            const dividerWidth = $(window).width() - dividerPosition.left;
             const bsGridGutter = 15;
-        
-        
-            if ( w > 768 ) {
-                obj.css( 'width', dividerWidth + bsGridGutter );
-                
-                if ( dir == 'left' ) {
-                    const _dis = -(dividerPosition.left + bsGridGutter*2 );
-                    obj.css( 'margin-left', _dis+'px' );
-                }  
+
+
+            if (w > 768) {
+                obj.css('width', dividerWidth + bsGridGutter);
+
+                if (dir == 'left') {
+                    const _dis = -(dividerPosition.left + bsGridGutter * 2);
+                    obj.css('margin-left', _dis + 'px');
+                }
 
             } else {
-                obj.css( 'width', 'inherit' );
-                
-                if ( dir == 'left' ) {
-                    obj.css( 'margin-left', -bsGridGutter + 'px' );
-                }    
-                
-                
+                obj.css('width', 'inherit');
+
+                if (dir == 'left') {
+                    obj.css('margin-left', -bsGridGutter + 'px');
+                }
+
+
             }
         }
-        
 
-		
-		
+
     };
 
-    module.components.pageLoaded.push( module.FULL_WIDTH_COLUMN_TO_EDGE.pageLoaded );
+    module.components.pageLoaded.push(module.FULL_WIDTH_COLUMN_TO_EDGE.pageLoaded);
 
-	return class FULL_WIDTH_COLUMN_TO_EDGE {
-		constructor() {
-			this.module = module;
-		}
-		
-	};
-	
-})( UixModuleInstance, jQuery, window, document );
+    return class FULL_WIDTH_COLUMN_TO_EDGE {
+        constructor() {
+            this.module = module;
+        }
+
+    };
+
+})(UixModuleInstance, jQuery, window, document);
 
 
 
